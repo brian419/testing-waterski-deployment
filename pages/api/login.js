36b6796c2712bd9cabe3f6dc08f7e960ad40307d
brian419 <json10@crimson.ua.edu>
2024-10-31 02:17:12 -0500

    trying to use POST in login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,67 +1,111 @@
-require('dotenv').config();
+// require('dotenv').config();
 
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+// const db = require('../../db');
+
+// module.exports.login = async (req, res) => {
+
+//     const { email, password } = req.body;
+
+//     db.query('SELECT * FROM User WHERE Email = ?', [email], async (err, results) => {
+//         if (err) {
+//             return res.status(500).json({ message: 'Database error' });
+//         }
+
+
+//         module.exports.login = async (req, res) => {
+//             const { email, password } = req.body;
+
+//             db.query('SELECT * FROM User WHERE Email = ?', [email], async (err, results) => {
+//                 if (err) {
+//                     return res.status(500).json({ message: 'Database error' });
+//                 }
+
+//                 if (results.length === 0) {
+//                     return res.status(400).json({ message: 'User not found' });
+//                 }
+
+//                 const user = results[0];
+//                 const validPassword = await bcrypt.compare(password, user.Password);
+//                 if (!validPassword) {
+//                     return res.status(400).json({ message: 'Invalid password or email' }); //we know it's invalid password but to not let malicious hackers know
+//                 }
+
+//                 const token = jwt.sign(
+//                     { id: user.CWID, email: user.Email },
+//                     process.env.JWT_SECRET,
+//                     { expiresIn: '1h' }
+//                 );
+
+
+
+//                 res.status(200).json({ message: 'Login successful', token });
+//             });
+//         };
+//         if (results.length === 0) {
+//             return res.status(400).json({ message: 'User not found' });
+//         }
+
+//         const user = results[0];
+//         const validPassword = await bcrypt.compare(password, user.Password);
+//         if (!validPassword) {
+//             return res.status(400).json({ message: 'Invalid password or email' }); //we know it's invalid password but to not let malicious hackers know
+//         }
+
+//         const token = jwt.sign(
+//             { id: user.CWID, email: user.Email },
+//             process.env.JWT_SECRET,
+//             { expiresIn: '1h' }
+//         );
+
+
+
+//         res.status(200).json({ message: 'Login successful', token });
+//     });
+// };
+
+
+
+
+require('dotenv').config();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const db = require('../../db');
 
 module.exports.login = async (req, res) => {
+    if (req.method !== 'POST') {
+        return res.status(405).json({ message: 'Method Not Allowed' });
+    }
 
     const { email, password } = req.body;
 
-    db.query('SELECT * FROM User WHERE Email = ?', [email], async (err, results) => {
-        if (err) {
-            return res.status(500).json({ message: 'Database error' });
-        }
+    try {
+        db.query('SELECT * FROM User WHERE Email = ?', [email], async (err, results) => {
+            if (err) {
+                return res.status(500).json({ message: 'Database error' });
+            }
 
+            if (results.length === 0) {
+                return res.status(400).json({ message: 'User not found' });
+            }
 
-        module.exports.login = async (req, res) => {
-            const { email, password } = req.body;
+            const user = results[0];
+            const validPassword = await bcrypt.compare(password, user.Password);
+            if (!validPassword) {
+                return res.status(400).json({ message: 'Invalid password or email' });
+            }
 
-            db.query('SELECT * FROM User WHERE Email = ?', [email], async (err, results) => {
-                if (err) {
-                    return res.status(500).json({ message: 'Database error' });
-                }
+            const token = jwt.sign(
+                { id: user.CWID, email: user.Email },
+                process.env.JWT_SECRET,
+                { expiresIn: '1h' }
+            );
 
-                if (results.length === 0) {
-                    return res.status(400).json({ message: 'User not found' });
-                }
-
-                const user = results[0];
-                const validPassword = await bcrypt.compare(password, user.Password);
-                if (!validPassword) {
-                    return res.status(400).json({ message: 'Invalid password or email' }); //we know it's invalid password but to not let malicious hackers know
-                }
-
-                const token = jwt.sign(
-                    { id: user.CWID, email: user.Email },
-                    process.env.JWT_SECRET,
-                    { expiresIn: '1h' }
-                );
-
-
-
-                res.status(200).json({ message: 'Login successful', token });
-            });
-        };
-        if (results.length === 0) {
-            return res.status(400).json({ message: 'User not found' });
-        }
-
-        const user = results[0];
-        const validPassword = await bcrypt.compare(password, user.Password);
-        if (!validPassword) {
-            return res.status(400).json({ message: 'Invalid password or email' }); //we know it's invalid password but to not let malicious hackers know
-        }
-
-        const token = jwt.sign(
-            { id: user.CWID, email: user.Email },
-            process.env.JWT_SECRET,
-            { expiresIn: '1h' }
-        );
-
-
-
-        res.status(200).json({ message: 'Login successful', token });
-    });
+            res.status(200).json({ message: 'Login successful', token });
+        });
+    } catch (error) {
+        console.error('Error logging in:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
 };
-
