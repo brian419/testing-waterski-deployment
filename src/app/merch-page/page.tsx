192bd4434f6e4ae13adb5bf5b54f92430b32fe96
brian419 <json10@crimson.ua.edu>
2024-10-30 19:43:29 -0500

    fixed any to strong types + wrapped fetchNotes with useCallBack to fix eslint issues

--- src/app/merch-page/page.tsx
+++ src/app/merch-page/page.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useState } from "react";
 import Link from 'next/link';
 import { fetchSheetData } from "../googlesheetservices";
-import Image from 'next/image';
 
 export default function MerchPage() {
     const [image2A, setImage2A] = useState<string | null>(null);
